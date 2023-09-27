1c3901dc7fa3d5406dc57397cd6354c65385f18b
j99445zf <zhihao.fang@student.manchester.ac.uk>
2023-09-27 14:38:05 +0800

    Add template

---
+++ mockup/hello.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+ <head>
+ <title>First Year Team Project</title>
+ </head>
+ <body>
+ <h1>W7</h1>
+
+<?php
+    echo("Hello World");
+    echo ("W7")
+?>
+
+ </body>
+</html>
+
